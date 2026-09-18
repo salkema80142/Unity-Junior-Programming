9afc01d2daf9136c5265eaa9e52a616a08b658b4
salkema80142 <salkema80@gmail.com>
2026-09-18 08:14:03 -0400

    1.2 Physics added
    Physics are added and the tank pushes the crates out of the way

--- Create with Code 1/Prototype/Assets/Scripts/PlayerController.cs
+++ Create with Code 1/Prototype/Assets/Scripts/PlayerController.cs
@@ -12,6 +12,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Move the vehicle forward
-        transform.Translate(Vector3.forward);
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
     }
 }
