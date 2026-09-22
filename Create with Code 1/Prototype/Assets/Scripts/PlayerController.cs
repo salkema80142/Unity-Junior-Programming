13e8ef3b6d5d7ff647e3a558e30d652dd624a871
salkema80142 <salkema80@gmail.com>
2026-09-22 08:20:55 -0400

    1.4.5 Debug the input values

--- Create with Code 1/Prototype/Assets/Scripts/PlayerController.cs
+++ Create with Code 1/Prototype/Assets/Scripts/PlayerController.cs
@@ -1,22 +1,27 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+
+
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
     public float turnSpeed;
     public InputAction moveAction;
-    
+    public Vector2 moveInput;
+
     // Start is called once before the first frame u[date
     void Start()
     {
-        
+        moveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
+        moveInput = moveAction.ReadValue<Vector2>();
+
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
         transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
     }
 }
