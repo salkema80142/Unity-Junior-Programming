ab3652ad5d8b2921555280c29e3f19402bbcab53
salkema80142 <salkema80@gmail.com>
2026-09-24 07:39:04 -0400

    Challenge 1 - The plane is going backward

--- Create with Code 1/My Plane Prototype 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Create with Code 1/My Plane Prototype 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
-    public float rotationSpeed;
+    public float speed = 5.0f;
+    public float rotationSpeed = 5.0f;
     public float verticalInput;
 
     // Start is called before the first frame update
@@ -15,15 +16,16 @@ public class PlayerControllerX : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right * rotationSpeed * verticalInput * Time.deltaTime);
+
     }
 }
